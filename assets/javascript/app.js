8819afb675b7e88bb8c2571c337e4ac32baec0ec
fzbo <elizabeth.bazan@gmail.com>
2017-11-28 17:05:21 -0500

    Latest revision.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,34 @@
 $(document).ready(function() {
 
+	$("#questionList").hide();
+	$("#results").hide();
+	$("display").hide();
 
+	var intervalid;
+	var number = 25;
+	var correct = 0;
+	var wrong = 0;
+	var noAnswer = 0;
+
+	function run() {
+		intervalid = setInterval(decrement, 1000);
+	}
+
+	function stop() {
+		clearInterval(intervalid);
+		$("#questionList").hide();
+		$("display").hide();
+		$("#results").show();
+	}
+
+	function decrement() {
+		number--;
+		$("#display").html("<h2>" + "Time Remaining: " + number + "</h2>");
+
+		if (number <=0) {
+			stop();
+		}
+	}
 
 
 
